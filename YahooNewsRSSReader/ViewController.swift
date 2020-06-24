569658e46dcec44f59319d89ba823c672d011e32
Junya Mizutani <mizutani@cyber.t.u-tokyo.ac.jp>
2020-06-24 10:48:30 +0900

    Initial Commit

---
+++ YahooNewsRSSReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YahooNewsRSSReader
+//
+//  Created by 水谷純也 on 2020/06/24.
+//  Copyright © 2020 水谷純也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
